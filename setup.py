7ad9fd67fd36e14f5be9ce913d1264e2682b3a90
Thomas Bille <thomas.bille@canonical.com>
2018-05-30 11:39:30 +0100

    Upgrade api to version 0.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='canonicalwebteam.snapstoreapi',
-    version='0.4.2',
+    version='0.4.3',
     author='Canonical Webteam',
     author_email='thomas.bille@canonical.com',
     url='https://github.com/canonical-webteam/snapstore-api',
